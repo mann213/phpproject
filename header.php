a6cc78dba491e0469b5e8d89e1065977c1204899
mann213 <75592950+mann213@users.noreply.github.com>
2021-02-14 14:34:19 -0800

    php website building from scratch

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+    <body>
+        <div align="left">
+            <li><a href="http://localhost/php7learning/ch2/index.php">Chapter 2</a></li>
+            <li><a href="ch11/index.php">Chapter 11</a></li>
+        <li><a href="ch12/index.php">Chapter 12</a></li> 
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include "header.php";
+        ?>
+        <div align="center">
+        <?php
+        echo "Hello PHP 7";
+        include "footer.php";
+        // put your code here
+        
+        ?>
+        </div>    
+            
+            
+    </body>
+</html>
